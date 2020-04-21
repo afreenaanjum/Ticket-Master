ea10ff17a7290efd564b62d1fa31660c741e470b
Afreena Anjum <anjumafreena@gmail.com>
2020-04-22 00:49:24 +0530

    Fixes

--- client/src/config/axios.js
+++ client/src/config/axios.js
@@ -1,9 +1,8 @@
 import Axios from 'axios'
 
 const axios = Axios.create({
-    baseURL: 'https://ticket-master-2019.herokuapp.com:' + process.env.PORT || 'http://localhost:3005'
+    baseURL: 'https://ticket-master-2019.herokuapp.com:' + process.env.PORT
+    // baseURL: 'http://localhost:3005'
 })
-
-const axios = Axios;
 
 export default axios
