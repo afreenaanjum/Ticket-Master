3b28f9affc6803581aa46ed1cc4fc6b1f52895d1
Afreena Anjum <anjumafreena@gmail.com>
2020-04-22 01:32:55 +0530

    Fixes

--- index.js
+++ index.js
@@ -7,9 +7,10 @@ const path = require('path');
 const app = express()
 
 
-app.use(express.json())
+
 app.use(cors())
 app.use('/', router)
+app.use(express.json())
 
 
 app.use(express.static(path.join(__dirname, 'client/build')));
